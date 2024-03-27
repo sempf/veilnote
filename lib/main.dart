94ecc5ebf61989b4b402359c67a6d68aaa44a125
Bill Sempf <bill@pointweb.net>
2024-03-26 21:54:12 -0400

    Created the Save action

--- lib/main.dart
+++ lib/main.dart
@@ -55,16 +55,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _saveText() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
@@ -114,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _saveText,
         tooltip: 'Save',
         child: const Icon(Icons.save),
       ), // This trailing comma makes auto-formatting nicer for build methods.
